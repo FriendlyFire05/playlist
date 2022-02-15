51f018921a0ffe17fc9d26218cd5d17c6851ae20
Glitch (bright-fluffy-hyacinth) <none>
2022-02-15 23:22:48 +0000

    🚛🗿 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
 // Complete Day 1 goals here
+let songName = ['1a','1b','1c','1d'];
+let songImage = ['2a','2b','2c','2d'];
+let songArtist = ['3a','3b','3c','3d'];
+let songLength = ['4a','4b','4c','4d'];
+let songLink = ['5a','5b','5c','5d'];
 
 function displaySongInfo() {
   // Complete the Day 2 goals inside this function
+  
+  songName.forEach(function(song)  {
+  //if($(".title").val() === song){
+    $(".songs").append(`<li>${song}</li>`) 
+  //  }
+  });
+  
+  songImage.forEach(function(image)  {
+  $(".images").append(`<li>${image}</li>`)  
+  });
+  
+  songArtist.forEach(function(artist)  {
+  $(".artists").append(`<li>${artist}</li>`)  
+  });
+  
+  songLength.forEach(function(length)  {
+  $(".lengths").append(`<li>${length}</li>`)  
+  });
+  
+  songLink.forEach(function(link)  {
+  $(".links").append(`<li>${link}</li>`)  
+  });
+  
 }
 
 function emptySongInfo() {
@@ -17,6 +45,7 @@ function emptySongInfo() {
 }
 
 function addSongInfo() {
+  let inputSong = $(".title").val();
   // Complete Day 3 goals inside this function
 }
 
